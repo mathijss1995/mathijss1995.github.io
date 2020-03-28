1340a4944cf1275bbf7aacd24148efc690aa36d5
mathijss1995 <62253519+mathijss1995@users.noreply.github.com>
2020-03-28 14:54:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ GROUND_Y = SCREEN_HEIGHT-(SCREEN_HEIGHT/10);
 PIPE_VELOCITY = -6;
 IS_DEAD = false;
 PIPE_BODY_HEIGHT = 70;
-PIPE_GAP_MIN = 3;
+PIPE_GAP_MIN = 1;
 PIPE_GAP_MAX = 5;
 PIPE_BOTTOM_MIN = GROUND_Y -100;
 SCALE = 0.5;
@@ -147,7 +147,8 @@ function draw() {
 
   if(frameCount % 100 == 0 && !IS_DEAD && STARTED){
 
-    gap = int(random(PIPE_GAP_MIN, PIPE_GAP_MAX)+50)
+    gap = int(random(PIPE_GAP_MIN, PIPE_GAP_MAX))
+    console.log('gap: ' + gap);
 
     //bottom pipes
     bottomBodies = int(random(PIPE_GAP_MAX, MAX_NUMBER_BODIES)) - gap
